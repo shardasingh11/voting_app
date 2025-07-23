58ee1ac91b4408f82af415d73d2228a548dd9172
Sharda Singh <shardasingh110011@gmail.com>
2025-07-23 11:08:05 +0530

    created routes for to give votes and see the list of the candidates

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -103,4 +103,91 @@ router.delete('/:candidateId',jwtAuthMiddleware, async (req, res) => {
     } 
 });
 
+// let's start voting
+router.post('/vote/:candidateId', jwtAuthMiddleware, async (req, res) => {
+    //no admin can vote
+    //user can vote only once
+
+    const candidateId = req.params.candidateId;
+    const userId = req.user.id;
+
+    try{
+        const candidate = await Candidate.findById(candidateId);
+        if(!candidate){
+            return res.status(404).json({message: "Candidate not found 👎"});
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: "User not found 👎"});
+        }
+
+        if(user.isVoted){
+            return res.status(400).json({message: "Sorry😒 You have already voted, can not vote again !"});
+        }
+        if(user.role === "admin"){
+            return res.status(403).json({message: "Admin not allowed to vote ❌"});
+        }
+
+        //Update the Candidate document to record the vote
+        candidate.votes.push({user: userId});
+        candidate.voteCount++;
+        await candidate.save();
+
+        //Update the user isVoted status
+        user.isVoted = true;
+        await user.save();
+
+        res.status(200).json({message: "Vote recorded successfully 🥳"});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error 😰"});
+    }
+
+});
+
+//GET route for count votes
+router.get('/vote/count', async (req, res) =>{
+    try{
+        //find all the candidates and sort them by voteCount in descending order
+        const candidates = await Candidate.find().sort({voteCount: 'desc'});
+
+        //Map the candidate to return their party name and voteCount
+        const voteRecord = candidates.map((data) => {
+            return {
+                party: data.party,
+                count: data.voteCount
+            }
+        });
+        return res.status(200).json(voteRecord);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+//GET route to show the list of candidates
+router.get('/candidates', async (req, res) => {
+    try{
+        const candidates = await Candidate.find();
+        console.log("candidate list", candidates);
+        if(!candidates)
+            return res.status(404).json({message: "No Candidate listed yet"});
+
+        const candidateList = candidates.map((data) => {
+            return {
+                name: data.name,
+                party: data.party,
+                age: data.age,
+                voteCount: data.voteCount
+            }
+        })
+        return res.status(200).json(candidateList);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+})
+
 module.exports = router;
